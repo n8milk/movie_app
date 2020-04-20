3ad86d3c24d63238a095133ed84414a2f79ce250
n8milk <n8milk@gmail.com>
2020-04-20 21:34:01 +0900

    Updates

---
+++ precache-manifest.098ed5dc140778daa04cb10f6f2bbb93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb14c72ecc6cbdb4d03074f6e725168",
+    "url": "/move_app/index.html"
+  },
+  {
+    "revision": "fd9405ec242155451767",
+    "url": "/move_app/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "98be54eaa9ed6a5a4f95",
+    "url": "/move_app/static/js/2.cfee456d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/move_app/static/js/2.cfee456d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd9405ec242155451767",
+    "url": "/move_app/static/js/main.ed899af3.chunk.js"
+  },
+  {
+    "revision": "15c34fa984a32f8cb472",
+    "url": "/move_app/static/js/runtime-main.0b3ecbfc.js"
+  }
+]);
